24d7bed1ff6cf74958f4f987f3242e632a70f5ae
Alex Siryi <desertkun@gmail.com>
2018-10-01 01:26:01 +0300

    Finally, fixed setup issue

--- setup.py
+++ setup.py
@@ -3,17 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypigit',
-    version='0.2.1',
+    version='0.2.2',
     description='A simple PyPi-like server that automatically generates python packages from git tags',
     author='desertkun',
     license='MIT',
     author_email='desertkun@gmail.com',
     url='https://github.com/anthill-utils/pypigit',
     packages=find_packages(),
-    include_package_data=True,
-    package_data={
-      "": ["templates/*.html"]
-    },
     zip_safe=False,
     install_requires=[
         "tornado>=5.0",
